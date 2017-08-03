4b0bcbe220a3236e3577e26d6b3a7aae47a3ff01
Clement <cmuhirwa@users.noreply.github.com>
2017-08-03 04:14:07 +0200

    gonow

--- api/testimg.php
+++ api/testimg.php
@@ -4,14 +4,14 @@
 	<title>Testing uploading</title>
 </head>
 <body>
-<form action="index.php" method="POST" enctype="multipart/form-data">
+<form action="testing.php" method="POST" enctype="multipart/form-data">
 	<input id="image" class="data" type="file" name="image" placeholder="optional image Here" onchange="checkPhoto(this)">
 	<input type="submit" value="uploading">
 </form>
 </body>
 </html>
 <?php
-include("testuploading.php");
+include("group.php");
 	if(isset($_FILES['image']['name'])){
 		$name=$_FILES['image']['name'];
 		$tmp=$_FILES['image']['tmp_name'];
