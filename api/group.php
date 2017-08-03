57bab33cb5bef7a42bb926d704c13760078476ed
Clement <cmuhirwa@users.noreply.github.com>
2017-08-03 04:02:30 +0200

    go

--- api/group.php
+++ api/group.php
@@ -1,7 +1,15 @@
 <?php
-$image=$_POST['image'];
-$name=$_POST['name'];
-$uploadpath="../groupimg/$name.jpg";
-file_put_contents($uploadpath,base64_decode($image));
-echo('image uploaded successfully');
+//$image=$_POST['image'];
+//$name=$_POST['name'];
+//$uploadpath="../groupimg/$name.jpg";
+//file_put_contents($uploadpath,base64_decode($image));
+//echo('image uploaded successfully');
+
+
+
+if ($_FILES['image']['tmp_name'] != "") {
+	    // Place image in the folder 
+	    move_uploaded_file($_FILES['fileField']['tmp_name'], "../groupimg/testing.jpg");
+	}
 ?>
+
