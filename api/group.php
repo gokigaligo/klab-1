b382724ca3c178f4c1e2c69c54d9753017107963
Clement <cmuhirwa@users.noreply.github.com>
2017-08-03 03:55:03 +0200

    xsa

--- api/group.php
+++ api/group.php
@@ -2,6 +2,12 @@
 $image=$_POST['image'];
 $name=$_POST['name'];
 $uploadpath="../groupimg/$name.jpg";
-file_put_contents($uploadpath,base64_decode($image));
-echo('image uploaded successfully');
+//file_put_contents($uploadpath,base64_decode($image));
+//echo('image uploaded successfully');
+
+if(!file_exists($_FILES['name']['tmp_name']) || !is_uploaded_file($_FILES['name']['tmp_name'])) {
+    echo 'No upload';
+}
 ?>
+
+
